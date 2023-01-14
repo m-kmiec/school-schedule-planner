8c3b31fb3b1b799d8817546d31b9ac31f15f6331
mklubowicz <michal.ghred@gmail.com>
2023-01-14 18:10:10 +0100

    delete and edit - WIP

--- src/courses/Courses.tsx
+++ src/courses/Courses.tsx
@@ -5,14 +5,27 @@ import MaterialTable from 'material-table';
 import CourseList from './CourseList';
 import AddCourse from './AddCourse';
 import Service from '../service/Service';
+import { get } from 'react-hook-form';
 
 function Courses() {
   const [courses, setCourses] = useState([] as Course[]);
 
   const [shownPage, setShownPage] = useState(PageEnum.list)
 
+  const [idToChange, setIdToChange] = useState<number | null>(null);
 
   useEffect(() => {
+    getAllCourses();
+  },[]);
+
+  useEffect(() => {
+    if(idToChange === null) return;
+    getAllCourses();
+    setIdToChange(null);
+  },[idToChange]);
+
+  const getAllCourses = () => {
+    console.log("test");
     Service.getCourses()
     .then((response: any) => {
       setCourses(response.data)
@@ -20,8 +33,7 @@ function Courses() {
     .catch((e: Error) => {
       console.log(e);
     });
-  },[]);
-  
+  }
 
   const onAddCourseClick = () => {
     setShownPage(PageEnum.add);
@@ -40,14 +52,15 @@ function Courses() {
   }
 
   const deleteCourse = (id: number) => {
-
+    Service.deleteCourse(id);
+    setIdToChange(id);
   }
 
   return (
     <div className="Courses">
       <h1>Courses</h1>
       {shownPage === PageEnum.list &&
-        <><CourseList list={courses} /*editCourse={editCourse} deleteCourse={deleteCourse}*/></CourseList>
+        <><CourseList list={courses} editCourse={editCourse} deleteCourse={deleteCourse}></CourseList>
           <input type="button" value="Add" onClick={onAddCourseClick} /></>
       }
       {shownPage === PageEnum.add && <AddCourse onBackButtonClick={showListPage} onSubmitClick={addCourse}/>}
