bb56e88fa46ec989fed32792f44b166f1dccb13f
Michal Kmiec <michal.kmiec01@gmail.com>
2023-01-15 19:16:27 +0100

    a bit of home page styling

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,7 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
 import { StudentGroup } from "./data/StudentGroups";
 import Service from "./service/Service";
+import "./Home.style.css";
 
 function Home() {
   const [studentGroups, setStudentGroups] = useState<StudentGroup[]>([]);
@@ -19,23 +20,26 @@ function Home() {
 
   return (
     <div>
-      <div>
+      <div className="title">
         <h1>Select students group:</h1>
       </div>
-      <ListGroup>
-        {studentGroups
-          ? studentGroups.map((studentGroup) => {
-              return (
-                <Link
-                  to={`schedule/${studentGroup.name}`}
-                  key={studentGroup.id}
-                >
-                  <ListGroup.Item>{studentGroup.name}</ListGroup.Item>
-                </Link>
-              );
-            })
-          : " "}
-      </ListGroup>
+      <div className="list">
+        <ListGroup>
+          {studentGroups
+            ? studentGroups.map((studentGroup) => {
+                return (
+                  <Link
+                    className="link"
+                    to={`schedule/${studentGroup.name}`}
+                    key={studentGroup.id}
+                  >
+                    <ListGroup.Item>{studentGroup.name}</ListGroup.Item>
+                  </Link>
+                );
+              })
+            : " "}
+        </ListGroup>
+      </div>
     </div>
   );
 }
